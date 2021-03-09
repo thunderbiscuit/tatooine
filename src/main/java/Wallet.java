74ad1b3879da4c07afab81fe52e28e5e57ade527
thunderbiscuit <thunderB@protonmail.com>
2021-03-09 17:38:04 -0500

    Feat: Add basic Java wallet class

---
+++ src/main/java/Wallet.java
@@ -0,0 +1,5 @@
+public class Wallet {
+    public void helloWallet() {
+        System.out.print("This wallet is alive and well");
+    }
+}
--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,13 +1,24 @@
 package com.goldenraven
 
+import Wallet
 import io.ktor.application.*
+import io.ktor.http.*
 import io.ktor.response.*
-import io.ktor.request.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    routing {
+        get("/") {
+            val myWallet: Wallet = Wallet()
+            myWallet.helloWallet()
+
+            call.respondText("Do. Or do not. There is no try.", ContentType.Text.Plain)
+        }
+    }
 }
 
