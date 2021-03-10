b0d473c9a869f0ddfc654a187ff40fd8999af6ae
thunderbiscuit <thunderB@protonmail.com>
2021-03-10 15:18:25 -0500

    Chore: Add Apache 2.0 license

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,4 +1,9 @@
-package com.goldenraven
+/*
+ * Copyright 2020 thunderbiscuit and contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the ./LICENSE file.
+ */
+ 
+ package com.goldenraven
 
 import Wallet
 import io.ktor.application.*
