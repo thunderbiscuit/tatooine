eb9c284d1b218745cf486e0f619a6e78a80e905d
thunderbiscuit <thunderbiscuit@protonmail.com>
2024-07-02 11:10:39 -0400

    Define custom amount in config file

--- app/src/main/kotlin/com/goldenraven/tatooine/Application.kt
+++ app/src/main/kotlin/com/goldenraven/tatooine/Application.kt
@@ -27,10 +27,11 @@ fun Application.module() {
     val descriptor = environment.config.property("wallet.descriptor").getString()
     val esploraUrl = environment.config.property("wallet.esploraUrl").getString()
     val electrumUrl = environment.config.property("wallet.electrumUrl").getString()
+    val amount = environment.config.property("wallet.amount").getString().toULong()
     val logger = LoggerFactory.getLogger("FAUCET_LOGS")
 
     // Initialize wallet
-    val faucetWallet = FaucetWallet(descriptor, electrumUrl)
+    val faucetWallet = FaucetWallet(descriptor, electrumUrl, amount)
     faucetWallet.sync()
 
     install(Authentication) {
